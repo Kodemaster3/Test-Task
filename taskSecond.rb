046a0af0b05202eecaba3f8e464c7d33c639b17c
Sergey Lielik <vivson66@gmail.com>
2021-01-03 21:33:44 +0200

    Update taskSecond.rb

--- taskSecond.rb
+++ taskSecond.rb
@@ -1,34 +1,25 @@
 class TaskSecond
-
-def some_method(text)
-words = []
-words = text.downcase.chomp.gsub(/[.,\/#!$%\^&\*;:{}=\-_~()?]/,"").split(" ")
-
-wo_re = {}
-i = 0
-while i < words.size do
-  unless wo_re.key?(words[i])
-    wo_re[words[i]] = 1
-  else
-    wo_re[words[i]] += 1
-  end
-  i += 1
-end
-
-if wo_re.size == words.size || words.size - wo_re.invert.size < 3
-  puts "Текст содержит меньше трёх уникальных слов"
-else
-  i = words.size - 1
-  ii = 3
-  while ii > 0 do
-    if wo_re.invert.key?(i)
-      print wo_re.invert[i] + " "
-      ii -= 1
+  def some_method(text)
+    words = []
+    words = text.downcase.chomp.gsub(/[.,\/#!$%\^&\*;:{}=\-_~()?]/,"").split(" ")
+    wo_re = {}
+    i = 0
+    while i < words.size do
+      unless wo_re.key?(words[i])
+        wo_re[words[i]] = 1
+      else
+        wo_re[words[i]] += 1
+      end
+      i += 1
     end
-    i -= 1
+
+    if wo_re.size == words.size || words.size - wo_re.invert.size < 3
+      puts "Текст содержит меньше трёх уникальных слов"
+    else
+      wor_e = wo_re.sort_by{ |key, value| value}
+      print wor_e[-1][0] + ", " + wor_e[-2][0] + ", " + wor_e[-3][0]
+    end
   end
-end
-end
 end
 
 test1 = TaskSecond.new
